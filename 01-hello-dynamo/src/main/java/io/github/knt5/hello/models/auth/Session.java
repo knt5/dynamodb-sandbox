dac395502ce6d8636e29eaedf425276833cca439
knt5 <knt5@users.noreply.github.com>
2017-01-27 00:35:02 +0900

    Update

--- 01-hello-dynamo/src/main/java/io/github/knt5/hello/models/auth/Session.java
+++ 01-hello-dynamo/src/main/java/io/github/knt5/hello/models/auth/Session.java
@@ -1,10 +1,10 @@
 package io.github.knt5.hello.models.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
+@AllArgsConstructor
 public class Session {
 	private String id;
 	private String userId;
--- 01-hello-dynamo/src/main/java/io/github/knt5/hello/services/auth/AuthService.java
+++ 01-hello-dynamo/src/main/java/io/github/knt5/hello/services/auth/AuthService.java
@@ -1,14 +1,23 @@
 package io.github.knt5.hello.services.auth;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
 
 import io.github.knt5.hello.models.auth.Session;
 
 @Service
 public class AuthService {
-	public Session login() {
-		Session session = new Session();
-		return session;
+	private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+	
+	public Session login(String userId, String password) {
+		// login
+		
+		
+		String sessionId = "xxx";
+		
+		return new Session(sessionId, userId, formatter.format(new Date()));
 	}
 	
 	public void touch(Session session) {
